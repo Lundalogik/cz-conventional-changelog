3d4c0e6e7a2e266e0d9dd0cc78b5b82b3266e968
Adrian Schmidt <adrian.schmidt@lime.tech>
2020-03-04 11:21:56 +0100

    feat(types): add support for custom types

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var configLoader = require('commitizen').configLoader;
 
 var config = configLoader.load();
 var options = {
-  types: conventionalCommitTypes.types,
+  types: Object.assign({}, conventionalCommitTypes.types, config.additionalTypes),
   defaultType: process.env.CZ_TYPE || config.defaultType,
   defaultScope: process.env.CZ_SCOPE || config.defaultScope,
   defaultSubject: process.env.CZ_SUBJECT || config.defaultSubject,
